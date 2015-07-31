340b7bd88e06bfb07ed480b9f72c32971885c3a7
saketomjaisawal <saket@eatechnologies.com>
2015-07-31 11:13:24 +0530

    assets added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( surveyor_all.css style.css prism.css surveyor_all.js )
+Rails.application.config.assets.precompile += %w( surveyor_all.js )
+Rails.application.config.assets.precompile << /materialize\/font\/.+\.(?:svg|eot|woff|woff2|ttf)\z/ 
